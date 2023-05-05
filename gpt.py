6bb60dbab226daa888d69696874cbb143c9910ea
c0nD <colintiller21@gmail.com>
2023-05-05 18:52:57 -0400

    multiple parallel processing

--- gpt.py
+++ gpt.py
@@ -3,16 +3,24 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # Hyper-parameters
-BATCH_SIZE = 32  # the amt of independent sequences to be processed in parallel
-BLOCK_SIZE = 8  # maximum context len for predictions
-MAX_ITER = 5500
+BATCH_SIZE = 64  # the amt of independent sequences to be processed in parallel
+BLOCK_SIZE = 256  # maximum context len for predictions
+MAX_ITER = 5000
 EVAL_INTERVAL = 500
-LEARNING_RATE = 1e-3
+LEARNING_RATE = 3e-4
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 EVAL_ITERS = 200
-N_EMBEDDING_DIM = 32
+N_EMBEDDING_DIM = 48  # 384 / 6 = 64 per head
+DROPOUT = 0.2
+NUM_HEADS = 3
+NUM_LAYER = 3  # change layers and embedding dimension if you want to scale it up/down
 
+
+# Printing to see if they're running GPU or CPU
+print('Using device:', DEVICE)
 # -------------------
+
+
 
 
 torch.manual_seed(420_69_1337)
@@ -64,6 +72,11 @@ def estimate_loss():
     model.train()
     return out
 
+
+
+
+
+
 # -------------------
 
 # one head of self-attention
@@ -77,6 +90,8 @@ class Head(nn.Module):
         self.query = nn.Linear(N_EMBEDDING_DIM, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(BLOCK_SIZE, BLOCK_SIZE)))  # buffer is a tensor that is not a parameter
         
+        self.dropout = nn.Dropout(DROPOUT)
+        
     def forward(self, x):
         B, T, C = x.shape
         k = self.key(x)  # (B,T,C)
@@ -86,6 +101,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2, -1) * (C**-.5)  # (B,T,C ) @ (B,C,T) = (B,T,T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))  # (B, T,T)
         wei = F.softmax(wei, dim=-1)  # (B,T,T)
+        wei = self.dropout(wei)  # (B,T,T)
         
         # weighted aggregation of vals
         v = self.value(x)  # (B,T,C)
@@ -99,27 +115,57 @@ class MultiHeadAttention(nn.Module):
     
     def __init__(self, n_heads, head_size):
         super().__init__()
-        self.heads = nn.ModuleList([Head(head_size) for _ in range(n_heads)])    
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(n_heads)])
+        self.proj = nn.Linear(n_heads * head_size, N_EMBEDDING_DIM)
+        self.dropout = nn.Dropout(DROPOUT)
         
         
     def forward(self, n):
         # n is (B,T,C)
-        return torch.cat([h(n) for h in self.heads], dim=-1)
+        out = torch.cat([h(n) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        return out
 
     
 # -------------------
 
+
+# allows each tkn to attend to all tkns in the context ( self-attention )
 class FeedForward(nn.Module):
     
     def __init__(self, N_EMBEDDING_DIM):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(N_EMBEDDING_DIM, N_EMBEDDING_DIM),
+            # paper has a dimensionality from 512 to 2048 (hence *4)
+            nn.Linear(N_EMBEDDING_DIM, N_EMBEDDING_DIM * 4),  
             nn.ReLU(),
+            nn.Linear(4 * N_EMBEDDING_DIM, N_EMBEDDING_DIM),  # projection layer back into residual pathway
+            nn.Dropout(DROPOUT),  # dropout layer
         )
         
     def forward(self, x):
         return self.net(x)
+    
+    
+# Transformer block (communication -> computation)
+class Block(nn.Module):
+    
+    def __init__(self, N_EMBEDDING_DIM, n_heads):
+        super().__init__()
+        head_size = N_EMBEDDING_DIM // n_heads
+        self.sa = MultiHeadAttention(n_heads, head_size)
+        self.frwd = FeedForward(N_EMBEDDING_DIM)
+        # (per token transformation that normailzes the output of the self-attention and feed-forward layers)
+        self.ln1 = nn.LayerNorm(N_EMBEDDING_DIM)
+        self.ln2 = nn.LayerNorm(N_EMBEDDING_DIM)
+        
+        
+        
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.frwd(self.ln2(x))
+        return x
+
     
     
 # Bigram language model
@@ -131,10 +177,10 @@ class Bigram(nn.Module):
         self.token_emb = nn.Embedding(vocab_size, N_EMBEDDING_DIM)
         # also take their position into account
         self.position_emb = nn.Embedding(BLOCK_SIZE, N_EMBEDDING_DIM)
-        self.sa_head = MultiHeadAttention(4, N_EMBEDDING_DIM//4)  # 4 communication channels of size 8 (8dim self-attention with 4 heads)
+        self.blocks = nn.Sequential(*[Block(N_EMBEDDING_DIM, n_heads=NUM_HEADS) for _ in range(NUM_LAYER)])
+        self.ln_f = nn.LayerNorm(N_EMBEDDING_DIM)
         self.lm_head = nn.Linear(N_EMBEDDING_DIM, vocab_size)
-        self.frwd = FeedForward(N_EMBEDDING_DIM)
-
+        
         
     def forward(self, idx, targets=None):
         B,T = idx.shape
@@ -143,8 +189,7 @@ class Bigram(nn.Module):
         pos_emb = self.position_emb(torch.arange(T, device=DEVICE))  # (T,C) -- positional embeddings
         tkn_embeddings = self.token_emb(idx) # (B,T,C)
         n = pos_emb + tkn_embeddings
-        x = self.sa_head(n)  # apply single head (B,T,C)
-        x = self.frwd(x)  # (B,T,C)
+        x = self.blocks(n)  # (B,T,C)
         logits = self.lm_head(x)  # (B,T,vcb_sz)      
         
         if targets is None:
